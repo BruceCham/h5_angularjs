c8bab3881d45dc1fe7ca6924a74fe74029f62315
tianyingchun <tianyingchun@outlook.com>
2015-01-23 15:23:09 +0800

    fix window bat execute

--- h5_angularjs_init.bat
+++ h5_angularjs_init.bat
@@ -2,19 +2,23 @@
 
 SET CWD=%~dp0
 
+REM use `CALL` keywords as only then the program control returns to the caller.
+
 REM install bower dependancy
 ECHO "bower install -> install project bower dependancy!\n"
-bower install
+CALL bower install
 
 REM install app theme dependancy!
 ECHO "bower isntall -> in /pafboots for theme dependancy! \n"
 CD pafboots
-bower install 
+CALL bower install 
 CD %CWD% 
 
 REM install app testing local server depandany.
 ECHO "npm install -> in /server for testing server dependancy! \n"
 CD server 
-npm install
+CALL npm install
 
 CD %CWD%
+
+PAUSE
