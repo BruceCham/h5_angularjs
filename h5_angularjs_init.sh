40271d081ed96d52d49ced4a1366e0a92d6aedac
tianyingchun <tianyingchun@outlook.com>
2015-01-23 14:11:30 +0800

    update power install

--- h5_angularjs_init.sh
+++ h5_angularjs_init.sh
@@ -1,14 +1,17 @@
-echo "`bower install ` install bower dependancy!\n"
+#! /bin/bash
+#
+
+echo "bower install -> install project bower dependancy!\n"
 # install bower dependancy
 bower install
 
 #install app theme dependancy!
-echo "`bower isntall` in /pafboots for theme dependancy! \n"
+echo "bower isntall -> in /pafboots for theme dependancy! \n"
 cd pafboots
 bower install 
 # install app testing local server depandany.
-echo "`npm install` in /server for testing server dependancy! \n"
-cd server 
+echo "npm install -> in /server for testing server dependancy! \n"
+cd ../server 
 npm install
 
 
